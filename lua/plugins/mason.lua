62b397eb02898d15c8d914c2b4699062ddb5337d
Paolo F. <paolofalcon98@gmail.com>
2026-02-10 07:51:38 -0500

    feat(mason): update mason ensure_installed elements for rust, python and javascript and terraform

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "mason-org/mason.nvim",
+        opts = {}
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+        opts = {
+            ensure_installed = {
+                "lua_ls",
+                "eslint",
+                "rust_analyzer",
+                "terraformls",
+                "ruff",
+                "basedpyright",
+            }
+        },
+    }
+}
