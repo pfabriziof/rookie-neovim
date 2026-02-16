07f41c2f248ec3472475a9cea1f162eb5bc4082e
Paolo F. <paolofalcon98@gmail.com>
2026-02-15 22:00:32 -0500

    feat(plugins,vimtex): add support for zathura forward search on wayland

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -9,6 +9,10 @@ return {
     init = function()
         -- VimTeX configuration goes here, e.g.
         vim.g.vimtex_view_method = "zathura"
+        vim.g.vimtex_view_forward_search_on_start = true
+        -- VimTeX can use D-Bus to communicate with Zathura on Wayland
+        -- without needing xdotool at all. Comment this if you're on X11.
+        vim.g.vimtex_view_zathura_options = '-x "nvim --remote-silent +%l %f"'
         vim.g.vimtex_compiler_latexmk = {
             out_dir = 'build',
         }
